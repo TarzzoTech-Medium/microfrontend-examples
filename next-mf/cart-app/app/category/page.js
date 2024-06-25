7a78f012233a9ec99be41413ba153623a4ed0215
Vijayasekhar Deepak <vijayasekhar.deepak@walmart.com>
2024-06-25 20:40:14 +0530

    removed nextjs mf

--- next-mf/cart-app/app/category/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function page() {
-  return (
-    <div className='text-3xl flex items-center justify-center mt-8 h-[100%]'>
-      Product Category
-    </div>
-  );
-}
--- next-mf/cart-app/app/page.js
+++
@@ -1,5 +0,0 @@
-import Products from '@/components/Products';
-
-export default function Home() {
-  return <Products />;
-}
